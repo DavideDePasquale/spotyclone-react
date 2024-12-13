06f2e775207b89f0eccf4c7b5e8cc5e3e42ab748
Davide De Pasquale <davide.depasquale1998@gmail.com>
2024-12-13 17:03:11 +0100

    fineeeeeeeeeeeeeeee (senza punto 4)

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,7 @@ import MainSection from "./components/MainSection";
 import TopMainSection from "./components/TopMainSection";
 import EndBar from "./components/EndBar";
 import MainSection2 from "./components/MainSection2";
-
 function App() {
-  const nomeArtista = "Lazza";
-  const nomeArtista2 = "Emis Killa";
-  const nomeArtista3 = "Eminem";
-  const genres = "Hip Hop Classic";
-  const genres2 = "Rap Classic";
-  const genres3 = "Rap God";
   return (
     <>
       <Container>
@@ -21,9 +14,9 @@ function App() {
           <SideBar />
           <Col xs={12} md={9} className="offset-md-3 mainPage">
             <TopMainSection />
-            <MainSection nomeArtista={nomeArtista} genres={genres} />
-            <MainSection nomeArtista={nomeArtista2} genres={genres2} />
-            <MainSection2 nomeArtista={nomeArtista3} genres={genres3} />
+            <MainSection nomeArtista="Lazza" genres="Hip Hop Classic" />
+            <MainSection nomeArtista="Emis Killa" genres="Rap Classic" />
+            <MainSection2 nomeArtista="Eminem" genres="Rap God" />
             <EndBar />
           </Col>
         </Row>
